302bca669224c968eba5a73716e4f9b872db42ed
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2021-10-11 19:35:38 +0100

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,12 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id:string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectActivity,
-    selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+    selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, 
+    createOrEdit, deleteActivity, submitting}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -26,6 +28,7 @@ export default function ActivityDashboard({activities, selectActivity,
                 activities={activities} 
                 selectActivity={selectActivity}
                 deleteActivity={deleteActivity}
+                submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -36,7 +39,12 @@ export default function ActivityDashboard({activities, selectActivity,
                         openForm={openForm}
                     />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}
+                />}
             </Grid.Column>
         </Grid>
     )
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,9 +7,10 @@ interface Props {
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props) {
 
     // ?? is Null-Coalescing Operator
     // The null-coalescing operator ?? returns the value of its left-hand operand if it isn't null; 
@@ -42,10 +43,10 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
                 <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange}  />
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange}  />
-                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}  />
+                <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}  />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}  />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}  />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='submit' content='Cancel' />
             </Form>
         </Segment>
