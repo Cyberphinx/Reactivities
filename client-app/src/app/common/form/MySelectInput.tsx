96ea054bead2dee2c76b8c508b2d35249f773e2a
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2021-10-18 18:38:07 +0100

    End of section 11

---
+++ client-app/src/app/common/form/MySelectInput.tsx
@@ -0,0 +1,31 @@
+import { useField } from "formik";
+import React from "react";
+import { Form, Label, Select } from "semantic-ui-react";
+
+interface Props {
+    placeholder: string;
+    name: string;
+    options: any;
+    label?: string;
+}
+
+export default function MySelectInput(props: Props) {
+    const [field, meta, helpers] = useField(props.name);
+    return (
+        // the double exclamation marks !! makes the object into a Boolean
+        <Form.Field error={meta.touched && !!meta.error}>
+            <label>{props.label}</label>
+            <Select 
+                clearable
+                options={props.options}
+                value={field.value || null}
+                onChange={(event, data) => helpers.setValue(data.value)}
+                onBlur={() => helpers.setTouched(true)}
+                placeholder={props.placeholder}
+            />
+            {meta.touched && meta.error? (
+                <Label basic color='red'>{meta.error}</Label>
+            ): null}
+        </Form.Field>
+    )
+}
